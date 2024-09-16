10dbccf2a7a6212eb61dbc8a744e9b7e8e95f57d
Augusto-Ildefonso <afildefonso16@gmail.com>
2024-09-16 15:11:13 -0300

    Atualizando arquivos

--- bruta/bruta.c
+++ bruta/bruta.c
@@ -1,3 +1,28 @@
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int N, W, peso, valor;
+    int** lista_itens, **mochila;
+
+    printf("Tamanho da mochila: ");
+    scanf("%d", &N);
+    printf("\n\nPeso máximo: ");
+    scanf("%d", &W);
+
+    for(int i = 0; i < N; i++){
+        printf("\nPeso | Valor\n");
+        scanf("%d %d", &peso, &valor);
+        printf("\n");
+
+        mochila[0][i] = lista_itens[0][i] = peso;
+        mochila[1][i] = lista_itens[1][i] = valor;
+    }
+        
+    for (int i = 0; i < N; i++){
+        
+    }
+    
+    
     return(0);
 }
