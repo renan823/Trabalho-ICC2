ce0c5f852ce53a7890f40c73a731b9f73cd00c19
Augusto-Ildefonso <afildefonso16@gmail.com>
2024-09-16 14:55:42 -0300

    Atualizando TAD mochila

--- bruta/mochila.c
+++ bruta/mochila.c
@@ -3,23 +3,24 @@
 #include <stdbool.h>
 
 struct mochila{
-    ITEM* itens;
+    ITEM** itens;
     float peso_max;
     int tam_max;
 };
 
-MOCHILA* mochila_criar(ITEM* itens, int W, int tam_max){
-    MOCHILA* mochila = (MOCHILA*) malloc(sizeof(MOCHILA) * tam_max);
+MOCHILA* mochila_criar(ITEM** itens, int W, int tam_max){
+    MOCHILA* mochila = (MOCHILA*) malloc(sizeof(MOCHILA));
 
     if(mochila != NULL){
         mochila->itens = itens;
         mochila->peso_max = W;
+        mochila->tam_max = tam_max;
     }
 
     return mochila;    
 }
 
-float mochila_atual_peso(MOCHILA* mochila){
+int mochila_atual_peso(MOCHILA* mochila){
     float peso = 0;
     for(int i = 0; i < mochila->tam_max; i++){
         peso += item_get_peso(mochila->itens[i]);
@@ -28,12 +29,12 @@ float mochila_atual_peso(MOCHILA* mochila){
     return peso;
 }
 
-float mochila_peso_max(MOCHILA* mochila){
+int mochila_peso_max(MOCHILA* mochila){
     return mochila->peso_max;
 }
 
 ITEM* mochila_itens(MOCHILA* mochila){
-    return mochila->itens;
+    return *(mochila->itens);
 }
 
 bool mochila_apagar(MOCHILA** mochila){
--- bruta/mochila.h
+++ bruta/mochila.h
@@ -5,9 +5,9 @@
 
     typedef struct mochila MOCHILA;
 
-    MOCHILA* mochila_criar(ITEM* itens, float W, int tam_max);
-    float mochila_atual_peso(MOCHILA* mochila);
-    float mochila_peso_max(MOCHILA* mochila);
+    MOCHILA* mochila_criar(ITEM** itens, int W, int tam_max);
+    int mochila_atual_peso(MOCHILA* mochila);
+    int mochila_peso_max(MOCHILA* mochila);
     ITEM* mochila_itens(MOCHILA* mochila);
     bool mochila_apagar(MOCHILA** mochila);
 
